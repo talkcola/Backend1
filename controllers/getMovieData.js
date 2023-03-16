a2515492dc70ef2d82c567331040b2aee6a8897b
seongwon-jang <seongwon.jang@digipen.edu>
2023-03-16 17:38:26 +0900

    Update getMovieData.js

--- controllers/getMovieData.js
+++ controllers/getMovieData.js
@@ -5,7 +5,7 @@ let movieData = [];
 request.get(
   "https://port-0-moviecrawl-9d7lbw2elczym3a7.gksl2.cloudtype.app/",
   function (err, response, body) {
-    console.log(body);
+    //console.log(body);
     movieData = body;
   }
 );
