c6c65e787e8c3d3084a922964415b9728c3765ee
seongwon-jang <seongwon.jang@digipen.edu>
2023-03-16 15:28:39 +0900

    test

--- controllers/getMovieData.js
+++ controllers/getMovieData.js
@@ -1,30 +1,29 @@
 // connectPython_print.js 파일
 
-let movie = (name,img,date) => {
-    return {
-        name: name,
-        img: img,
-        date: date
-    }
-}
+let movie = (name, img, date) => {
+  return {
+    name: name,
+    img: img,
+    date: date,
+  };
+};
 
-const spawn = require('child_process').spawn;
+const spawn = require("child_process").spawn;
 
-const result = spawn('python', ['index.py']);
+const result = spawn("python", ["index.py"]);
 let movieData = [];
-result.stdout.on('data', function(data) {
-    let a = data.toString().split('\n');
-    for(let i =2; i < a.length; i += 3)
-    {
-        movieData.push(movie(a[i-2],a[i-1],a[i]));
-        //console.log(`제목: ${a[i-2]}`);
-        //console.log(`사진: ${a[i-1]}`);
-        //console.log(`날짜: ${a[i]}`);
-    }
+result.stdout.on("data", function (data) {
+  let a = data.toString().split("\n");
+  for (let i = 2; i < a.length; i += 3) {
+    movieData.push(movie(a[i - 2], a[i - 1], a[i]));
+    //console.log(`제목: ${a[i-2]}`);
+    //console.log(`사진: ${a[i-1]}`);
+    //console.log(`날짜: ${a[i]}`);
+  }
 });
 
-result.stderr.on('data', function(data) {
-    console.log(data.toString());
+result.stderr.on("data", function (data) {
+  console.log(data.toString());
 });
 
 module.exports = movieData;
